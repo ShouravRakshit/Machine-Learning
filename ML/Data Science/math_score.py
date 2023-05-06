67c389a263a110cc612fd2ddbc95a282bcc851cd
ShouravRakshit <69850843+ShouravRakshit@users.noreply.github.com>
2023-05-06 01:05:04 -0600

    .

--- ML/Data Science/math_score.py
+++ ML/Data Science/math_score.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -15,19 +18,58 @@ for high_score in range(len(data["Higher_Score"])):
 for high_score in range(len(data["Higher_Score"])):
     data["Higher_Score"][high_score] *= data["Higher_Score"][high_score]
 
-# print(data)
+print(data)
 
-lsd_ppm = list(data["LSD_ppm"])
-avg_math_score = list(data["Avg_Math_Test_Score"])
+# lsd_ppm = list(data["LSD_ppm"])
+# avg_math_score = list(data["Avg_Math_Test_Score"])
 # print(lsd_ppm)
 # print(avg_math_score)
 
-clean_data = {'LSD_ppm': lsd_ppm, 'Avg_Math_Test_Score': avg_math_score}
+# clean_data = {'LSD_ppm': lsd_ppm, 'Avg_Math_Test_Score': avg_math_score}
 
-df = pd.DataFrame(clean_data)
+# df = pd.DataFrame(clean_data)
 # print(df)
-del data["Higher_Score"]
+# del data["Higher_Score"]
 # print(data)
 
-e = math.e
-print(e)
+title_font = {
+    "fontsize": 15,
+    "color": "orange",
+    "horizontalalignment": "center"
+}
+
+x_label_font = {
+    "fontsize": 15,
+    "color": "blue",
+    "horizontalalignment": "center"
+}
+
+y_label_font = {
+    "fontsize": 15,
+    "color": "blue",
+    "horizontalalignment": "center"
+}
+
+# e = math.e
+
+plt.figure(figsize=(7, 7))
+time_delay_minutes = data[["Time_Delay_in_Minutes"]]
+lsd_ppm = data[["LSD_ppm"]]
+avg_math_score = data[["Avg_Math_Test_Score"]]
+
+x = np.array(lsd_ppm)
+y = np.array(avg_math_score)
+# print([x])
+# print(y)
+regression = LinearRegression()
+# To get the fit of the graph we need independent and dependant variable.
+regression.fit(x, y)
+print(regression.coef_)         # Slope of the graph.
+print(regression.intercept_)    # Intercept of the graph.
+print(regression.score(x, y))
+plt.title(label="Tissue concentration of LSD over time", fontdict=title_font)
+plt.xlabel(xlabel="LSD (ppm)", fontdict=x_label_font)
+plt.ylabel(ylabel="Math Score", fontdict=y_label_font)
+plt.text(x=0, y=.3, s="Wagner et al. (1968)")
+plt.plot(x, y, color="red", linewidth=4)
+plt.show()
--- ML/Regression/linear_regression.py
+++ ML/Regression/linear_regression.py
@@ -1,47 +1,77 @@
-import pandas
-import matplotlib.pyplot as plt
+# import pandas
+# import matplotlib.pyplot as plt
+# from sklearn.linear_model import LinearRegression
+#
+# data = pandas.read_csv("cost_revenue_clean.csv")
+# # This gives us max, min, count and other stuffs about data.
+# print(data.describe())
+#
+# font1 = {'family': 'serif',
+#          'color': 'blue',
+#          'size': 10
+#          }
+# font2 = {'family': 'serif',
+#          'color': 'darkred',
+#          'size': 12
+#          }
+#
+#
+# plt.figure(figsize=(10, 6))
+# plt.xlabel("Production_Budget", fontdict=font1)
+# plt.ylabel("Worldwide_Gross_Budget", fontdict=font1)
+# plt.title("Production Budget Vs Worldwide Gross Budget", fontdict=font2)
+#
+# X = pandas.DataFrame(data, columns=["production_budget"])
+# Y = pandas.DataFrame(data, columns=["worldwide_gross_budget"])
+# plt.scatter(X, Y, alpha=.4)
+# plt.ylim(0, 3000000000)
+# plt.xlim(0, 450000000)
+#
+# regression = LinearRegression()
+#
+# regression.fit(X, Y)
+# print(regression.coef_)         # Slope of the graph.
+# print(regression.intercept_)    # Intercept of the graph
+#
+# # This line plots the line on the graph. Here we are using the value of X as X because we are using it as
+# # independent variable. We are using the same X value we are getting from the csv sheet. Now, if we use the same value
+# Y
+# # value of the sheet it would not make any sense. So, in order to have relationship between these two variables we are
+# # going to use the X values to get the predicted Y values. We already know the slope and intercept
+# # from X and previous Y value.
+#
+# # This line will plot the graph on the background.
+# plt.plot(X, regression.predict(X), color="green", linewidth=3)
+# print(regression.score(X, Y))
+# # We need this line to show the graph on the screen.
+# plt.show()
+#
+
+
+import pandas, matplotlib.pyplot as plot
 from sklearn.linear_model import LinearRegression
 
 data = pandas.read_csv("cost_revenue_clean.csv")
-# This gives us max, min, count and other stuffs about data.
-print(data.describe())
+# print(data)
+# print(data.describe())
 
-font1 = {'family': 'serif',
-         'color': 'blue',
-         'size': 10
-         }
-font2 = {'family': 'serif',
-         'color': 'darkred',
-         'size': 12
-         }
+X = pandas.DataFrame(data, columns=["production_budget_usd"])
+y = pandas.DataFrame(data, columns=["worldwide_gross_usd"])
 
-
-plt.figure(figsize=(10, 6))
-plt.xlabel("Production_Budget", fontdict=font1)
-plt.ylabel("Worldwide_Gross_Budget", fontdict=font1)
-plt.title("Production Budget Vs Worldwide Gross Budget", fontdict=font2)
-
-X = pandas.DataFrame(data, columns=["production_budget"])
-Y = pandas.DataFrame(data, columns=["worldwide_gross_budget"])
-plt.scatter(X, Y, alpha=.4)
-plt.ylim(0, 3000000000)
-plt.xlim(0, 450000000)
-
+print(X)
+print(y)
 regression = LinearRegression()
+regression.fit(X, y)
 
-regression.fit(X, Y)
-print(regression.coef_)         # Slope of the graph.
-print(regression.intercept_)    # Intercept of the graph
-
-# This line plots the line on the graph. Here we are using the value of X as X because we are using it as
-# independent variable. We are using the same X value we are getting from the csv sheet. Now, if we use the same value Y
-# value of the sheet it would not make any sense. So, in order to have relationship between these two variables we are
-# going to use the X values to get the predicted Y values. We already know the slope and intercept
-# from X and previous Y value.
-
-# This line will plot the graph on the background.
-plt.plot(X, regression.predict(X), color="green", linewidth=3)
-print(regression.score(X, Y))
-# We need this line to show the graph on the screen.
-plt.show()
-
+print(regression.coef_)
+print(regression.intercept_)
+print(regression.score(X,y))
+plot.figure(figsize=(10, 6))
+plot.scatter(X, y, alpha=.5)
+plot.plot(X, regression.predict(X), color='red', linewidth=5)
+plot.title("Film cost Vs Global Revenue")
+plot.xlabel("Production Budget $")
+plot.ylabel("Worldwide Gross $")
+plot.xlim(0, 450000000)
+plot.ylim(0, 3000000000)
+plot.show()
