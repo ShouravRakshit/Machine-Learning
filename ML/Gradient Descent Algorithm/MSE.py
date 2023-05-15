31c4205832f697a4e49649e31a674963c3150bcf
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-05-15 16:54:26 -0600

    update

--- ML/Gradient Descent Algorithm/MSE.py
+++ ML/Gradient Descent Algorithm/MSE.py
@@ -29,4 +29,28 @@ y_predicted_vals = .84753515 + 1.22272646 * x
 
 MSE = mse(y, y_predicted_vals)
 print("The value of MSE is :", MSE)
+
+th_0 = np.linspace(start=-1, stop=3, num=500)
+th_1 = np.linspace(start=-1, stop=3, num=500)
+
+plot_t0, plot_t1 = np.meshgrid(th_0, th_1)
+
+plot_cost = np.zeros((500, 500))  # It creates 2d array
+
+for i in range(500):
+    for j in range(5):
+        y_hat = plot_t0[i][j] + plot_t1[i][j] * x
+        plot_cost[i][j] = mse(y, y_hat)
+
+print(plot_cost)
+
+# Plotting MSE
+fig = plt.figure(figsize=[16, 12])
+ax = fig.add_subplot(projection='3d')
+ax.set_xlabel("Theta 0", fontsize=20)
+ax.set_ylabel("Theta 1", fontsize=20)
+ax.set_zlabel("Cost - MSE", fontsize=20)
+
+ax.plot_surface(plot_t0, plot_t1, plot_cost)
+
 plt.show()
