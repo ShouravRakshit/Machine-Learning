cd1259a1c000bd41c5177eca4a4c9e8966b6769d
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-05-20 00:55:21 -0600

    .

--- ML/Multiple Regression/Online Market Multi Variable Regression.py
+++ ML/Multiple Regression/Online Market Multi Variable Regression.py
@@ -4,4 +4,24 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("online.csv")
-print(data["Area"])
+dummy = pd.get_dummies(data["Area"])
+dummy_df = dummy.astype(int)
+
+merged_data = pd.concat([data, dummy_df], axis=1)
+
+x = merged_data.drop(['Area', 'Profit'], axis=1)
+y = merged_data[["Profit"]]
+
+regression = LinearRegression()
+regression.fit(x, y)
+
+print("This is the value of intercept:", regression.intercept_)
+print("This is the value of slope:", regression.coef_)
+
+# Select a specific column from x for the scatter plot
+x_column = x.columns[0]
+plt.figure(figsize=(10, 5))
+plt.scatter(x[x_column], y)
+plt.xlabel(x_column)
+plt.ylabel("Profit")
+plt.show()
